0f6732f8d2258841b47f26484485aba8f6b7f2dc
Ishan Chakraborty <ishanchakraborty2496@gmail.com>
2025-12-11 18:52:56 +0530

    Update homepage and base path for deployment
    Changed the homepage URL in package.json and the base path in vite.config.js to reflect the new project name and deployment location.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Custom Domain configured: 'ishanchakraborty.com' (Requires root base)
-  base: "/portfolio-app/",
+  base: "/ishan-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
